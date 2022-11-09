cf28232b2fe993b31286c3e1f9999332538ce442
DavidBg <odga95@gmail.com>
2022-11-08 22:43:32 -0500

    Template Tabs

--- themes/www/source/js/general.js
+++ themes/www/source/js/general.js
@@ -1,39 +1,63 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
 
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+//Variables
+const cambiaPestaña = document.querySelector("#tabs-list");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	// Cuando se da click en alguna pestaña de template se cambia la pestaña
+	cambiaPestaña.addEventListener('click', mostrarContenido);
+
+});
+
+
+function mostrarContenido(e) {
+	e.preventDefault();
+	// Mostrar el contenido y cambiar la pestaña de templates
+	if (!e.target.classList.contains('tab-select')) {
+		var select = document.querySelector(".tab-select");
+		select.classList.remove("tab-select"); // Se elimina la clase existente 
+		e.target.classList.add("tab-select"); // Se agrega la clase del tab seleccionado 
+		var content = e.target.id;
+		const tabContent = document.querySelector(".tab-content__" + content); // se busca el contenido por clase
+		// se muestra el contenido de la clase seleccionada y oculta la actual.
+		if (!tabContent.classList.contains("enabled")) {
+			var enabled = document.querySelector(".enabled");
+			tabContent.classList.remove("disabled");
+			tabContent.classList.add("enabled");
+			enabled.classList.add("disabled");
+			enabled.classList.remove("enabled");
+		}
+	}
 }
-resize();
-window.onresize = resize;
 
-function noise(ctx) {
-    
-    var w = ctx.canvas.width,
-        h = ctx.canvas.height,
-        idata = ctx.createImageData(w, h),
-        buffer32 = new Uint32Array(idata.data.buffer),
-        len = buffer32.length,
-        i = 0;
+// SLIDER BASIC
+var slideIndex = 1;
+showDivs(slideIndex);
 
-    for(; i < len;)
-        buffer32[i++] = ((100 * Math.random())|0) << 24;
-    
-    ctx.putImageData(idata, 0, 0);
+function plusDivs(n) {
+    showDivs(slideIndex += n);
 }
 
-var toggle = true;
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
 
-// added toggle to get 30 FPS instead of 60 FPS
-(function loop() {
-    toggle = !toggle;
-    if (toggle) {
-        requestAnimationFrame(loop);
-        return;
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlide");
+    var y = document.getElementsByClassName("mySlideText");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
     }
-    noise(ctx);
-    requestAnimationFrame(loop);
-})();
-
-
+    x[slideIndex - 1].style.display = "block";
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    if(y.length !==0){
+        y[slideIndex - 1].style.display = "block";
+    }else{
+        console.log("nada")
+    }
+}
